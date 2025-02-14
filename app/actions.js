17ab2ac229f85e0306e7d1a4f965baa283f0be77
PavelShpagin <pavelandrewshpagin@gmail.com>
2025-02-14 14:26:47 +0200

    remove redirects

--- app/actions.js
+++ app/actions.js
@@ -6,9 +6,6 @@ export async function loginWithDiscord() {
   const supabase = createClient();
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "discord",
-    options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_BASE_URL}/xx/auth/callback`,
-    },
   });
 
   if (error) {
